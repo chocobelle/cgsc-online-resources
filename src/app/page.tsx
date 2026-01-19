5ca862785ef897992a832a22decd6761b05a709c
ʚɞ Isabelle <209904419+chocobelle@users.noreply.github.com>
2026-01-19 23:30:29 +0000

    Worked on the header area

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,8 @@ export default function MainEntry() {
   return (
     <>
       <nav className="flex justify-between items-center py-3 px-4 font-zalando border-b border-oliveGreen">
-
         <div className="logoSide">
-            <img src="/favicon.png" alt="site logo" className="w-8"/>
+          <img src="/favicon.png" alt="site logo" className="w-8" />
           <h1 className="font-fraunces text-2xl font-medium">
             CGSC Online Resources
           </h1>
@@ -25,7 +24,15 @@ export default function MainEntry() {
         </ul>
       </nav>
 
-      <header></header>
+      <header>
+        <img src="/hg.png" alt="His Grace" className="left" />
+        <img src="/leaf-1.png" alt="leaf graphic" className="right" />
+        <h2>Welcome To CGSC Online Resources</h2>
+        <p>
+          An unofficial, volunteer-run website providing resources for followers
+          of Christ the Good Shepherd Church across the world!
+        </p>
+      </header>
       <main></main>
       <footer></footer>
     </>
